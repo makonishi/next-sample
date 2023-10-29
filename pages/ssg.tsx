36d90479b233fe80035015775ed24db72fb43a66
Makoto Konishi <mkonishi@cuon.co.jp>
2023-10-29 20:59:15 +0900

    static site generatio by getStaticProps function

--- pages/ssg.tsx
+++ pages/ssg.tsx
@@ -1,9 +1,15 @@
-import { NextPage } from "next";
+import { GetStaticProps, NextPage, NextPageContext } from "next";
 import Head from "next/head";
 
-type SSGProps = {}
+type SSGProps = {
+  message: string
+}
 
-const SSG: NextPage<SSGProps> = () => {
+// getStaticPropsが返したpropsをSSGは受け取る
+// NextPage<SSGProps>はmessage: stringのみを受け取って生成されるページの型
+const SSG: NextPage<SSGProps> = (props) => {
+  const { message } = props
+
   return (
     <div>
       <Head>
@@ -14,9 +20,21 @@ const SSG: NextPage<SSGProps> = () => {
         <p>
           This Page is a page generated at build time by ssg.
         </p>
+        <p>{message}</p>
       </main>
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps<SSGProps> = async (context) => {
+  const timestamp = new Date().toLocaleString()
+  const message = `${timestamp}にgetStaticPropsが実行されました`
+  console.log(message)
+  return {
+    props: {
+      message,
+    }
+  }
 }
 
 export default SSG
